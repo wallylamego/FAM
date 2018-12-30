5e68792a135d25c678fc71bf6c767309941b5b52
WallyF <wallylamego@hotmail.com>
2018-12-30 23:16:22 +0200

    Put Javascript in a seperate file

--- WebAppFAM/wwwroot/js/site.js
+++ WebAppFAM/wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿// Write your Javascript code.
+function cleanSpaces(inputString) {
+    //Remove all Line Breaks
+    var newStr = inputString.replace(/(\r\n|\n|\r)/gm, "");
+    //Remove all double spaces and replace with one space
+    var newStr1 = newStr.replace(/\s+/g, " ");
+    //Remove leading and trailing spaces
+    var newStr2 = $.trim(newStr1);
+    return newStr2;
+}
