f378221be2bb004170c5f407c85096fd7a6c0e91
WallyF <wallylamego@hotmail.com>
2019-01-01 12:55:59 +0200

    Customer Pages Complete and Added

--- WebAppFAM/Pages/Commodities/Create.cshtml.cs
+++ WebAppFAM/Pages/Commodities/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,6 +10,7 @@ using WebAppFAM.Models;
 
 namespace WebAppFAM.Pages.Commodities
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly WebAppFAM.Models.WebAppFAMContext _context;
--- WebAppFAM/Pages/Customers/Index.cshtml.cs
+++ WebAppFAM/Pages/Customers/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using WebAppFAM.Models;
 
 namespace WebAppFAM
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly WebAppFAM.Models.WebAppFAMContext _context;
@@ -19,7 +21,76 @@ namespace WebAppFAM
         }
 
         public IList<Customer> Customer { get;set; }
+        public async Task<JsonResult> OnPostPaging([FromForm] DataTableAjaxPostModel Model)
+        {
 
+            int filteredResultsCount = 0;
+            int totalResultsCount = 0;
+
+            DataTableAjaxPostModel.GetOrderByParameters(Model.order, Model.columns, "Name",
+                out bool SortDir, out string SortBy);
+
+
+            //First create the View of the new model you wish to display to the user
+            var CustomerQuery = _context.Customers
+               .Select(c => new
+               {
+                   c.CustomerID,
+                   CustomerName = c.Name,
+                   c.AccountNo
+               }
+               );
+
+            totalResultsCount = CustomerQuery.Count();
+            filteredResultsCount = totalResultsCount;
+
+            if (!string.IsNullOrEmpty(Model.search.value))
+            {
+                CustomerQuery = CustomerQuery
+                        .Where(
+                c => c.CustomerName.ToLower().Contains(Model.search.value.ToLower())
+                       );
+
+                filteredResultsCount = CustomerQuery.Count();
+            }
+            var Result = await CustomerQuery
+                        .Skip(Model.start)
+                        .Take(Model.length)
+                        .OrderBy(SortBy, SortDir)
+                        .ToListAsync();
+
+            var value = new
+            {
+                // this is what datatables wants sending back
+                draw = Model.draw,
+                recordsTotal = totalResultsCount,
+                recordsFiltered = filteredResultsCount,
+                data = Result
+            };
+            return new JsonResult(value);
+        }
+
+        public async Task<IActionResult> OnDeleteDelete([FromBody] Customer obj)
+        {
+
+            if (obj != null && HttpContext.User.IsInRole("Admin"))
+            {
+                try
+                {
+                    _context.Customers.Remove(obj);
+                    await _context.SaveChangesAsync();
+                    return new JsonResult("Customer removed successfully");
+                }
+                catch (DbUpdateException d)
+                {
+                    return new JsonResult("Customer not removed." + d.InnerException.Message);
+                }
+            }
+            else
+            {
+                return new JsonResult("Customer not removed.");
+            }
+        }
         public async Task OnGetAsync()
         {
             Customer = await _context.Customers.ToListAsync();
