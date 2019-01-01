b51085276eae729cbb9b905b6a087c83dd20d9fb
WallyF <wallylamego@hotmail.com>
2019-01-01 13:34:33 +0200

    SubContractor and Status Pages Completed

--- WebAppFAM/Pages/SubContractors/Index.cshtml.cs
+++ WebAppFAM/Pages/SubContractors/Index.cshtml.cs
@@ -19,7 +19,76 @@ namespace WebAppFAM.Pages.SubContractors
         }
 
         public IList<SubContractor> SubContractor { get;set; }
+        public async Task<JsonResult> OnPostPaging([FromForm] DataTableAjaxPostModel Model)
+        {
 
+            int filteredResultsCount = 0;
+            int totalResultsCount = 0;
+
+            DataTableAjaxPostModel.GetOrderByParameters(Model.order, Model.columns, "Name",
+                out bool SortDir, out string SortBy);
+
+
+            //First create the View of the new model you wish to display to the user
+            var SubContractorQuery = _context.SubContractor
+               .Select(s => new
+               {
+                   s.SubContractorID,
+                   SubContractorName = s.Name,
+                   s.AccountNo
+               }
+               );
+
+            totalResultsCount = SubContractorQuery.Count();
+            filteredResultsCount = totalResultsCount;
+
+            if (!string.IsNullOrEmpty(Model.search.value))
+            {
+                SubContractorQuery = SubContractorQuery
+                        .Where(
+                c => c.SubContractorName.ToLower().Contains(Model.search.value.ToLower())
+                       );
+
+                filteredResultsCount = SubContractorQuery.Count();
+            }
+            var Result = await SubContractorQuery
+                        .Skip(Model.start)
+                        .Take(Model.length)
+                        .OrderBy(SortBy, SortDir)
+                        .ToListAsync();
+
+            var value = new
+            {
+                // this is what datatables wants sending back
+                draw = Model.draw,
+                recordsTotal = totalResultsCount,
+                recordsFiltered = filteredResultsCount,
+                data = Result
+            };
+            return new JsonResult(value);
+        }
+
+        public async Task<IActionResult> OnDeleteDelete([FromBody] SubContractor obj)
+        {
+
+            if (obj != null && HttpContext.User.IsInRole("Admin"))
+            {
+                try
+                {
+                    _context.SubContractor.Remove(obj);
+                    await _context.SaveChangesAsync();
+                    return new JsonResult("SubContractor removed successfully");
+                }
+                catch (DbUpdateException d)
+                {
+                    return new JsonResult("SubContractor not removed." + d.InnerException.Message);
+                }
+            }
+            else
+            {
+                return new JsonResult("SubContractor not removed.");
+            }
+        }
         public async Task OnGetAsync()
         {
             SubContractor = await _context.SubContractor.ToListAsync();
